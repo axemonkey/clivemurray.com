aa36e1266fed65e557af048a038f6551a66b97da
axemonkey <hi+github@clivemurray.com>
2023-03-22 12:21:33 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+import {clivemurrayApp} from './clivemurray.com/app.js';
+
+const env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+	clivemurrayApp
+		.listen(7777);
+} else if (env === 'production') {
+	clivemurrayApp
+		.listen(process.env.PORT); // host specifies port number
+}
