3915a54b2a44498e53ca5d462ea9d12075ec52a6
axemonkey <hi+github@clivemurray.com>
2024-02-28 12:10:06 +0000

    tweaks inc make 404 work

--- _site/public/js/es5bundle.main.js
+++ _site/public/js/es5bundle.main.js
@@ -3,7 +3,7 @@
 
 	const straps = ['I play guitar, you know', 'Now in colours!', 'display: bloke;', 'It’s good for you', 'HACKA LÖKEN!', 'Always running', 'Web stuff since 1997', 'Disinformation superlayby', 'At the forefront of the retreat', 'More harmonies', 'Do you have any grey poupon?', 'He’s beginning to believe', 'It’s not the years, it’s the mileage', 'Be excellent to each other', 'Alsø wik', 'Alsø alsø wik', 'I know where Bruce Lee lives', 'It’s all relative', 'JEM777LNG #407', 'Never put a sock in a toaster', 'Never put jam on a magnet', 'Thou shalt not question Stephen Fry', 'Cough drop and roll'];
 	const sinclairResearch = year => {
-	  document.querySelector('.copyright').innerHTML = "&copy; ".concat(year, " Clive Murray Research Ltd");
+	  document.querySelector('.copyright').innerHTML = `&copy; ${year} Clive Murray Research Ltd`;
 	};
 	const writeStrapline = thisMonth => {
 	  if (thisMonth === 2) {
