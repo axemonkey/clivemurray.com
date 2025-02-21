c5139c0f8920545b19c6ccae9d1989e959af28af
axemonkey <hi+github@clivemurray.com>
2025-02-21 09:55:05 +0000

    rename src folder

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,11 +1,11 @@
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy('clivemurray.com/public');
-	eleventyConfig.addPassthroughCopy({ 'clivemurray.com/robots.txt': '/robots.txt' });
+	eleventyConfig.addPassthroughCopy('src/public');
+	eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
 	eleventyConfig.setUseGitIgnore(false);
 	eleventyConfig.setServerOptions({
 		// liveReload: false,
 		watch: [
-			'clivemurray.com/public/**/*',
+			'src/public/**/*',
 		],
 		showVersion: true,
 	});
